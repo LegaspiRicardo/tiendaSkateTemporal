7a5fe39ac3c390f4525bf83b40dd1d3e489166dd
Legaspi Ricardo <108900203+LegaspiRicardo@users.noreply.github.com>
2023-03-06 23:19:00 -0600

    Actualizacion Website

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -164,39 +164,6 @@
         </div>
     </div>
 
-    <div class="row carrusel-noticias">
-        <div id="carouselExampleIndicators" class="carousel slide col-12 mx-auto" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="images/skatePark1.jpg" class="d-block img-carrusel-noticias" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="images/skateFriends.jpg" class="d-block img-carrusel-noticias" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="images/skatePerfil.jpg" class="d-block img-carrusel-noticias" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="images/vansYcalceta.jpg" class="d-block img-carrusel-noticias" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </button>
-        </div>
-    </div>
-
     <div class="row carrusel-ropa">
         <div class="col-5 text-center texto-carrusel-tablas">
             <h2>Playeras favoritas</h2>
@@ -319,6 +286,41 @@
         </div>
     </div>
 
+    <!-- <div class="row carrusel-noticias">
+        <div id="carouselExampleIndicators" class="carousel slide col-12 mx-auto" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/skatePark1.jpg" class="d-block img-carrusel-noticias" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/skateFriends.jpg" class="d-block img-carrusel-noticias" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/skatePerfil.jpg" class="d-block img-carrusel-noticias" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/vansYcalceta.jpg" class="d-block img-carrusel-noticias" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </button>
+        </div>
+    </div> -->
+
+
+
     <div class="row tendencias-carrusel ">
         <img src="images/vigas.jpg" alt="" class="imagen-fondo-tendencias ">
 
@@ -417,7 +419,22 @@
         </div>
     </div>
 
+
+
+    <div class="row text-center blog ">
+
+        <div class="col-4 ml-auto p-0">
+            <img src="images/tablaCuadros.jpg" alt="" class="img-blog">
+            <h3 class="btn-blog">Blog</h3>
+        </div>
+        <div class="col-4 mr-auto  p-0">
+            <img src="images/skating.jpg" alt="" class="img-blog">
+            <h3 class="class=" btn-blog">Eventos</h3>
+        </div>
+    </div>
+
     <div class="row marcas-carrusel ">
+        <img src="images/vigas.jpg" alt="" class="imagen-fondo-marcas ">
         <div class="col-8 mx-auto">
 
             <h2 class="text-center mb-5">Marcas disponibles</h2>
@@ -457,33 +474,23 @@
                         </div>
                     </div>
 
-                    <a class="carousel-control-prev" href="#carouselBrandsControls" role="button" data-slide="prev">
+                    <!-- <a class="carousel-control-prev" href="#carouselBrandsControls" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="sr-only">Previous</span>
                     </a>
                     <a class="carousel-control-next" href="#carouselBrandsControls" role="button" data-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="sr-only">Next</span>
-                    </a>
+                    </a> -->
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="row text-center blog ">
-
-        <div class="col-4 ml-auto p-0">
-            <img src="images/tablaCuadros.jpg" alt="" class="img-blog">
-            <h3 class="btn-blog">Blog</h3>
-        </div>
-        <div class="col-4 mr-auto  p-0">
-            <img src="images/skating.jpg" alt="" class="img-blog">
-            <h3 class="class=" btn-blog">Eventos</h3>
-        </div>
-    </div>
 
 
 
+    
     <div class="row contacto-form">
         <div class="col-6 ml-auto ">
             <div class="col-10 mx-auto">
@@ -534,8 +541,6 @@
 
 
     </div>
-
-
 </main>
 
 
--- resources/views/website/piezas/navbarDash.blade.php
+++ resources/views/website/piezas/navbarDash.blade.php
@@ -1,28 +1,35 @@
 <nav class="navbarDash navbar-expand-lg row m-0" id="navbar">
     <div class="col-3 text-center">
-        <a class="navbar-brand" href="{{ url('/tiendaSkate') }}">Radical Boards logo</a>
+        <a class="navbar-brand align-self-center" href="{{ url('/tiendaSkate') }}">Radical Boards logo</a>
     </div>
 
-    <div class="col-9 " >
-        <ul class="navbar-nav col-6 ml-auto mt-2">
-            <li class="nav-item mr-5">
+    <div class="col d-flex justify-content-end" >
+        <ul class="navbar-nav col-7 d-flex justify-content-around mt-2">
+            <li class="nav-item text-center">
                 <a class="nav-link" href="{{ url('/tiendaSkate') }}">Inicio <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item mr-5">
+            <li class="nav-item text-center">
                 <a class="nav-link" href="{{ url('productos') }}">Productos</a>
             </li>
-            <li class="nav-item mr-5">
+            <li class="nav-item text-center">
                 <a class="nav-link" href="#">Link</a>
             </li>
-            <li class="nav-item mr-5">
+            <li class="nav-item text-center">
                 <a class="nav-link" href="#">Link</a>
             </li>
-            <li class="nav-item ">
+            <li class="nav-item text-center">
                 <a class="nav-link pl-0 pr-0" href="{{ url('sesion') }}">Inicia sesión</a>
             </li>
         </ul>
     </div>
 </nav>
+
+
+
+
+
+
+
 
 <script>
     // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
@@ -44,7 +51,7 @@
         }
 
         if (document.body.scrollTop >= 1300 || document.documentElement.scrollTop >= 1300) {
-            document.getElementById("navbar").style.backgroundColor = "#212426";
+            document.getElementById("navbar").style.backgroundColor = "#3c4245";
             document.getElementById("navbar").style.top = "0";
             document.getElementById("navbar").style.zIndex = "1";
         } 
